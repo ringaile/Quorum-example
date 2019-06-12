0355cda5c22ced5ff31a874bece28eaf5cb962e7
ringaile <ringaile@gmail.com>
2019-06-12 11:45:17 +0200

    added readme

--- quorumProject/contracts/SimpleStorage.sol
+++ quorumProject/contracts/SimpleStorage.sol
@@ -2,15 +2,15 @@ pragma solidity >=0.5.0;
 
 contract SimpleStorage {
 // declare variable
-  string storedData;
+  uint storedData;
     
   
-  function set(string memory x) public {
+  function set(uint x) public {
   // Store the string inside the Contract
         storedData = x;
   }
     
-  function get() public view returns (string memory retVal) {
+  function get() public view returns (uint) {
   // Retrieve the value when calling the function
         return storedData;
   }
